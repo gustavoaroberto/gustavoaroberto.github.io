eefc262d0d797a28544e13c4e3fa877ef18a38a0
Gustavo Roberto <gustavoa_roberto@icloud.com>
2022-08-02 22:43:18 -0300

    static folder + site url

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,14 +1,21 @@
 module.exports = {
   siteMetadata: {
-    title: `gus website`,
-    siteUrl: `https://www.yourdomain.tld`
+    title: `Gus Website`,
+    siteUrl: `https://www.gustavoaroberto.me`,
   },
-  plugins: ["gatsby-plugin-styled-components", "gatsby-plugin-image", "gatsby-plugin-sitemap", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "images",
-      "path": "./src/images/"
+  plugins: [
+    "gatsby-plugin-styled-components",
+    "gatsby-plugin-image",
+    "gatsby-plugin-sitemap",
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "images",
+        path: "./src/images/",
+      },
+      __key: "images",
     },
-    __key: "images"
-  }]
+  ],
 };
